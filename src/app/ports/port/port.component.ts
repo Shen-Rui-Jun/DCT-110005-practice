f5594f2c91d794c9a74dcfa1a736f810e904dc51
John <john.shen@cloudysys.com>
2021-04-24 13:51:18 +0800

    任務3 講師講解改進

--- src/app/ports/port/port.component.ts
+++ src/app/ports/port/port.component.ts
@@ -1,8 +1,10 @@
+import { SingleArticle } from './../../interfaces/single-article';
 import { Article } from './../../interfaces/article';
 import { PostService } from './../../post.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, switchMap } from 'rxjs/operators'
+import { Observable } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-port',
@@ -11,15 +13,26 @@ import { map, switchMap } from 'rxjs/operators'
 })
 export class PortComponent implements OnInit {
 
-  article: Article;
+  // article: Article;
+
+  // 更優的做法, 就是善用 RxJS
+  article$: Observable<Article>;
 
   constructor(private route: ActivatedRoute, private postService: PostService) { }
 
   ngOnInit(): void {
-    this.route.paramMap.pipe(
+    // this.route.paramMap.pipe(
+    //   map(paramMap => paramMap.get('id')),
+    //   switchMap(id => this.postService.getArticle(id))
+    // ).subscribe(result => { this.article = result.article; })
+
+    // 更優的做法, 就是善用 RxJS
+    this.article$ = this.route.paramMap.pipe(
       map(paramMap => paramMap.get('id')),
-      switchMap(id => this.postService.getArticle(id))
-    ).subscribe(result => { this.article = result.article; })
+      switchMap(id => this.postService.getArticle(id)),
+      map(singleArticle => singleArticle.article),
+      // shareReplay(1), // 類似 cache, 避免重複呼叫 API
+    );
 
   }
 
--- src/app/ports/ports.component.ts
+++ src/app/ports/ports.component.ts
@@ -1,7 +1,8 @@
+import { Observable } from 'rxjs';
 import { PostService } from './../post.service';
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../interfaces/article';
-
+import { map, shareReplay, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ports',
@@ -10,13 +11,21 @@ import { Article } from '../interfaces/article';
 })
 export class PortsComponent implements OnInit {
 
-  articles: Article[];
+  // articles: Article[];
+
+  // 更優的做法, 就是善用 RxJS
+  articles$: Observable<Article[]>;
 
   constructor(private postService: PostService) { }
 
   ngOnInit(): void {
-    this.postService.getArticles().subscribe(result => {
-      this.articles = result.articles
-    });
+    // this.postService.getArticles().subscribe(result => {
+    //   this.articles = result.articles
+    // });
+
+    // 更優的做法, 就是善用 RxJS
+    this.articles$ = this.postService.getArticles().pipe(
+      map(result => result.articles)
+    );
   }
 }
